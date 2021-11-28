62135b7b334e39dd4cdc7c13d417293bae00be73
Ariethel <a.renzullo9@studenti.unisa.it>
2021-11-28 22:17:03 +0100

    vm ubuntu

--- SistemiOperativi/libso2021/Debug/src/subdir.mk
+++ SistemiOperativi/libso2021/Debug/src/subdir.mk
@@ -4,20 +4,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/CountProgram.c 
+../src/error.c \
+../src/resource.c 
 
 OBJS += \
-./src/CountProgram.o 
+./src/error.o \
+./src/resource.o 
 
 C_DEPS += \
-./src/CountProgram.d 
+./src/error.d \
+./src/resource.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/usr/local/include -includeourhdr.h -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
