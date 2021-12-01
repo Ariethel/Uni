a708935a8e5a5f8c991c87851c9d012511101171
Ariethel <a.renzullo9@studenti.unisa.it>
2021-12-01 22:58:11 +0100

    capito come configurare il jar per MySql

--- BasiDiDati/Lezione1-12.IntroduzioneJDBC/src/ExampleJDBC.java
+++ BasiDiDati/Lezione1-12.IntroduzioneJDBC/src/ExampleJDBC.java
@@ -1,21 +1,44 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ExampleJDBC {
 	
 	//Definisco alcune stringhe che mi servono per la connessione al database
-	public final String DB_URL = "JDBC:mysql://localhost/JDBCTUTORIAL";
-	public final String USER = "Guest";
-	public final String PASSWORD = "Guest123";
-	public final String QUERY = "select id,first,last,age from employees"; //Query da effettuare
+	public final static String DB_URL = "JDBC:mysql://localhost/unisa2022";
+	public final static String USER = "root";
+	public final static String PASSWORD = "passwd";
+	public final static String QUERY = "select nome,cognome,stipendio from impiegati"; //Query da effettuare
 	
 	public static void main(String[] args) {
+		//Controllo sia caricato il driver per connettermi a MySQL
 		try{
-			Connection conn = DriverManager.getConnection(DB_URL);
-		} catch (Exception e) {
-			// TODO: handle exception
+		    Class.forName("com.mysql.jdbc.Driver");
 		}
+		catch (ClassNotFoundException e){
+		    e.printStackTrace();
+		}
+		
+		
+		
+		try{ 
+			Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD); //Creo la connessione
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(QUERY); //Eseguo la query e carico i risultati in rs
+			while(rs.next()) { //stampo i vari risultati della query
+				System.out.println("Nome: " + rs.getString("nome"));
+				System.out.println("Cognome: " + rs.getString("cognome"));
+				System.out.println("Stipendio: " + rs.getString("Stipendio"));
 
+			}
+			
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
 	}
-
+		 
 }
+
