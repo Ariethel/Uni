c7186e95bbe22570c66374e4d449c49685ea4401
Ariethel <a.renzullo9@studenti.unisa.it>
2021-12-03 23:10:43 +0100

    boh roba

---
+++ Programmazione ObjOrtd/EserciziJavaIO.02-12/src/TestImbarcazioni.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestImbarcazioni {
+	
+	public static void main(String args[]) throws FileNotFoundException {
+		File fp = new File("input.dat");
+		Scanner in = new Scanner(fp);
+		while(in.hasNext()) {
+			
+		}
+	}
+}
