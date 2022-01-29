8921559b4eac559a67eb8c902ae24b87c621974f
Ariethel <a.renzullo9@studenti.unisa.it>
2022-01-29 14:32:15 +0100

    so

--- SistemiOperativi/Esame13-01-2022/src/Esame13-01-2022.c
+++ SistemiOperativi/Esame13-01-2022/src/Esame13-01-2022.c
@@ -9,18 +9,18 @@
  */
 
 #include <ourhdr.h>
+#include <dirent.h>
 int pid[4];
 
 void worker(){
-	srand(time(NULL));
 	int i = 0;
 	while (i < 10000){
 		int rand = random() % 1001;
 		char path[256];
 		sprintf(path, "%d.txt", rand);
-		printf("int: %d\n Path: %s", rand, path);
+		printf("int: %d \t Path: %s\n", rand, path);
 		int fp;
-		if ((fp = open(path, O_CREAT| O_WRONLY)) < 0) err_sys("Write failed in children");
+		if (((fp = open(path, O_CREAT| O_RDWR)) < 0) && !path) err_sys("Write failed in children");
 		close(fp);
 		usleep(rand);
 		i++;
@@ -36,9 +36,25 @@ int main(void) {
 			exit(1);
 		}
 	}
-
 	for (int i = 0; i < 4; i++){
-		wait(NULL);
+			wait(NULL);
 	}
+	DIR *dir;
+	int sum = 0, count = 0;
+	char path[256] = "/home/amnesia/Uni/SistemiOperativi/Esame13-01-2022";
+	if ((dir = opendir(path)) == NULL) err_sys("failed opening %s\n",path);
+	struct dirent *entry;
+	while((entry = readdir(dir)) != NULL){
+		int num = entry->d_name + '0';
+		if (num > 500){
+			sum += num;
+			count++;
+			printf("Cancellato %s\n", entry->d_name);
+			remove(entry->d_name);
+		}
+
+	}
+	closedir(dir);
+	printf("Media: %f", sum/count);
 	return EXIT_SUCCESS;
 }
