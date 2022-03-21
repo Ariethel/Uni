c01fdd1698aa8737787cbdfc22ff7ee9764003d8
Ariethel <a.renzullo9@studenti.unisa.it>
2022-03-21 20:36:26 +0100

    push

--- TechWeb/8.Approfondimento Servlet, Cookie, Session/EsercizioSession/src/main/java/controller/ServletSession.java
+++ TechWeb/8.Approfondimento Servlet, Cookie, Session/EsercizioSession/src/main/java/controller/ServletSession.java
@@ -4,13 +4,26 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ServletSession" , value = "/index.html")
 public class ServletSession extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse res){
+    private int numAccessi = 0;
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
+        HttpSession ssn = req.getSession();
+        ssn.setAttribute("NumAccessi", Integer.toString(numAccessi));
 
+        PrintWriter out = res.getWriter();
 
+        if (ssn != null){
+            numAccessi++;
+            ssn.setAttribute("NumAccessi", Integer.toString(numAccessi));
+            out.println("Accessi: " + ssn.getAttribute("NumAccessi"));
+        }
 
     }
 }
