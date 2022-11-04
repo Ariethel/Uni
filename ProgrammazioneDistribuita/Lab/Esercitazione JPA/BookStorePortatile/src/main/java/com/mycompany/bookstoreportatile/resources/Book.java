2c952e000083b44b43f65d4ed09ddae1a25752ba
Ariethel <a.renzullo9@studenti.unisa.it>
2022-11-04 15:27:11 +0100

    boh io ci sto provando

---
+++ ProgrammazioneDistribuita/Lab/Esercitazione JPA/BookStorePortatile/src/main/java/com/mycompany/bookstoreportatile/resources/Book.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bookstoreportatile.resources;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author amnesia
+ */
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String isbn;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (isbn != null ? isbn.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the isbn fields are not set
+        if (!(object instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) object;
+        if ((this.isbn == null && other.isbn != null) || (this.isbn != null && !this.isbn.equals(other.isbn))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.bookstoreportatile.resources.Book[ id=" + isbn + " ]";
+    }
+    
+}
